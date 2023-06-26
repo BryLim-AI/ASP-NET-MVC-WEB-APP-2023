5a3cb0925f1005a006d8fc3b981ba7fc579584d6
BryLim-AI <katoshi.bryan@gmail.com>
2023-06-26 09:15:34 +0800

    Qr Code

---
+++ Controllers/QrCodeController.cs
@@ -0,0 +1,47 @@
+﻿
+using IronBarCode;
+using Microsoft.AspNetCore.Mvc;
+using QrCode.Models;
+
+
+namespace QrCode.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public QrCodeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(QrCodeModel qrCode)
+        {
+            try
+            {
+                GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(qrCode.QRUrl, 200);
+                barcode.AddBarcodeValueTextBelowBarcode();
+                // Style
+                barcode.SetMargins(10);
+                barcode.ChangeBarCodeColor(Color.CadetBlue);
+                string path = Path.Combine(_environment.WebRootPath, "GeneratedQRCode/qrcode.png");
+                barcode.SaveAsPng(path);
+                string fileName = Path.GetFileName(path);
+                string imageUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}" + "/GeneratedQRCode/" + fileName;
+                ViewBag.QrCodeUri = imageUrl;
+
+                
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return View(qrCode);
+        }
+
+        
+    }
+}
---
+++ Models/QrCodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QrCode.Models
+{
+
+    public class QrCodeModel
+    {
+        public QrCodeModel()
+        {
+            QRUrl = "";
+        }
+        [Display(Name= "Enter URL Here:")]
+        public string QRUrl { get; set; }
+
+        [Display(Name = "QRCode Image")]
+        public string QRCodeImagePath { get; set; }
+    }
+}
